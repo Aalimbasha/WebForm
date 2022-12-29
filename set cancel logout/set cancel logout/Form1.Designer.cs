2a881908666563529359f45080c308de45317e0e
Aalimbasha <114008953+Aalimbasha@users.noreply.github.com>
2022-12-29 18:38:10 +0530

    First Commit

---
+++ set cancel logout/set cancel logout/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace set_cancel_logout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.BackColor = System.Drawing.SystemColors.Window;
+            this.lbl.Font = new System.Drawing.Font("Arial Narrow", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(178, 73);
+            this.lbl.Name = "lbl";
+            this.lbl.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbl.Size = new System.Drawing.Size(165, 48);
+            this.lbl.TabIndex = 0;
+            this.lbl.Text = "Hello World";
+            this.lbl.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lbl.UseCompatibleTextRendering = true;
+            this.lbl.UseMnemonic = false;
+            this.lbl.UseWaitCursor = true;
+            this.lbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnLogout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogout.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLogout.Location = new System.Drawing.Point(510, 233);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(90, 34);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Log out";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSet.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSet.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSet.Location = new System.Drawing.Point(205, 233);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(90, 34);
+            this.btnSet.TabIndex = 4;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = false;
+            this.btnSet.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Red;
+            this.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCancel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCancel.Location = new System.Drawing.Point(362, 233);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(90, 34);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.DimGray;
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btnReset.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnReset.Location = new System.Drawing.Point(49, 233);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnReset.Size = new System.Drawing.Size(90, 34);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.UseWaitCursor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.lbl);
+            this.groupBox1.Location = new System.Drawing.Point(30, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(598, 264);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Multi Label";
+            this.groupBox1.UseCompatibleTextRendering = true;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(660, 358);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.groupBox1);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ set cancel logout/set cancel logout/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace set_cancel_logout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            lbl.Text = "Welcome to windows";
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            
+            lbl.Text = "Cancelled Successfully";
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+           
+            lbl.Text = "Log Out Successfully";
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+           
+            lbl.Text = "Hello World";
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
